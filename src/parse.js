83d82bb68b33836ca08230cb9a103a029677ecce
Cristian Gabbanini <cristian@iMac-di-Cristian.local>
2019-05-31 18:32:02 +0200

    Add parsing

--- src/parse.js
+++ src/parse.js
@@ -2,8 +2,55 @@ const { isOpeningParenthesis, isClosingParenthesis } = require('./identify');
 const { specialForms } = require('./special-forms');
 const { peek, pop } = require('./utilities');
 
-const parenthesize = () => {};
+const parenthesize = tokens => {
+  const token = pop(tokens);
 
-const parse = () => {};
+  if (isOpeningParenthesis(token.value)) {
+    const expression = [];
+
+    while (!isClosingParenthesis(peek(tokens).value)) {
+      expression.push(parenthesize(tokens));
+    }
+
+    pop(tokens);
+    return expression;
+  }
+
+  return token;
+};
+
+const parse = tokens => {
+  if (Array.isArray(tokens)) {
+    const [first, ...rest] = tokens;
+    return {
+      type: 'CallExpression',
+      name: first.value,
+      arguments: rest.map(parse),
+    };
+  }
+
+  const token = tokens;
+
+  if (token.type === 'Number') {
+    return {
+      type: 'NumericLiteral',
+      value: token.value,
+    };
+  }
+
+  if (token.type === 'String') {
+    return {
+      type: 'StringLiteral',
+      value: token.value,
+    };
+  }
+
+  if (token.type === 'Name') {
+    return {
+      type: 'Identifier',
+      name: token.value,
+    };
+  }
+};
 
 module.exports = { parse: tokens => parse(parenthesize(tokens)) };
