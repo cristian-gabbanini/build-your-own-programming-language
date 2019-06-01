611c3ae7d0cb88eb8ffe9e7418d8c081b2450df2
Cristian Gabbanini <cristian.gabbanini@gmail.com>
2019-06-01 07:53:56 +0200

    Add ast traversal and compile to javascript

--- src/traverse.js
+++ src/traverse.js
@@ -1,3 +1,27 @@
-const traverse = (node, visitor) => {};
+const traverseNode = ({ node, parent, visitor }) => {
+  const { enter, exit } = visitor[node.type] || {};
+
+  if (enter) {
+    enter({ node, parent });
+  }
+
+  if (node.arguments) {
+    traverseArray({ array: node.arguments, parent: node, visitor });
+  }
+
+  if (exit) {
+    exit({ node, parent });
+  }
+};
+
+const traverseArray = ({ array, parent, visitor }) => {
+  array.forEach(node => {
+    traverseNode({ node, parent, visitor });
+  });
+};
+
+const traverse = (node, visitor) => {
+  traverseNode({ node, visitor });
+};
 
 module.exports = { traverse };
--- tests/traverse.test.js
+++ tests/traverse.test.js
@@ -1,7 +1,7 @@
 const { traverse } = require('../src/traverse');
 
 describe(traverse, () => {
-  it.skip('should travel to all the nodes in the tree and reverse the math', () => {
+  it('should travel to all the nodes in the tree and reverse the math', () => {
     const ast = {
       type: 'CallExpression',
       name: 'add',
@@ -31,7 +31,7 @@ describe(traverse, () => {
     expect(ast.name).toBe('subtract');
   });
 
-  it.skip('should travel to all the nodes in the tree and double all of the numbers', () => {
+  it('should travel to all the nodes in the tree and double all of the numbers', () => {
     const ast = {
       type: 'CallExpression',
       name: 'add',
@@ -53,5 +53,51 @@ describe(traverse, () => {
 
     expect(ast.arguments[0].value).toBe(24);
     expect(ast.arguments[1].value).toBe(12);
+  });
+
+  it('should change all the strings into Captain America', () => {
+    const ast = {
+      type: 'StringLiteral',
+      value: 'Hello world!',
+      arguments: [{ type: 'StringLiteral', value: 'Hello world!' }],
+    };
+
+    const visitor = {
+      StringLiteral: {
+        exit({ node }) {
+          node.value = 'Captain America';
+        },
+      },
+    };
+
+    traverse(ast, visitor);
+
+    expect(ast.value).toBe('Captain America');
+    expect(ast.arguments[0].value).toBe('Captain America');
+  });
+
+  it('should modify the tree', () => {
+    const ast = {
+      type: 'CallExpression',
+      name: '+',
+      arguments: [
+        { type: 'NumericLiteral', value: 12 },
+        { type: 'NumericLiteral', value: 6 },
+      ],
+    };
+
+    const visitor = {
+      CallExpression: {
+        enter({ node }) {
+          if (node.name === '+') {
+            node.name = 'add';
+          }
+        },
+      },
+    };
+
+    traverse(ast, visitor);
+
+    expect(ast.name).toBe('add');
   });
 });
