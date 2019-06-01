3d07d17448f0734d62326ba7e3fb30242c1b33d0
Cristian Gabbanini <cristian.gabbanini@gmail.com>
2019-06-01 08:27:44 +0200

    Transpile variable declarations to javascript

--- tests/to-javascript.test.js
+++ tests/to-javascript.test.js
@@ -21,4 +21,17 @@ describe(toJavaScript, () => {
 
     expect(toJavaScript(ast)).toBe('add(2, 3, subtract(5, 4))');
   });
+
+  it('should support variable declaration', () => {
+    const ast = {
+      type: 'VariableDeclaration',
+      identifier: {
+        type: 'Identifier',
+        name: 'x',
+      },
+      assignment: { type: 'NumericLiteral', value: 2 },
+    };
+
+    expect(toJavaScript(ast)).toBe('let x = 2;');
+  });
 });
